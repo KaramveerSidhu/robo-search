d54430d27329fe8bb4048d3350f25e30607a29b9
KaramveerSidhu <karamveersidhu14@gmail.com>
2020-07-21 16:14:00 +0530

    Updates

---
+++ precache-manifest.f60007a2e6985606c51fb5bf2f9a2a47.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "945333ffa90cd79c3d5d456eff3a69f0",
+    "url": "/KaramveerSidhu/robo-search/index.html"
+  },
+  {
+    "revision": "4cf38c8163921d162759",
+    "url": "/KaramveerSidhu/robo-search/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "8dad13e39a910e4ff512",
+    "url": "/KaramveerSidhu/robo-search/static/css/main.229a2e4e.chunk.css"
+  },
+  {
+    "revision": "4cf38c8163921d162759",
+    "url": "/KaramveerSidhu/robo-search/static/js/2.6b4e3719.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/KaramveerSidhu/robo-search/static/js/2.6b4e3719.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8dad13e39a910e4ff512",
+    "url": "/KaramveerSidhu/robo-search/static/js/main.3d112979.chunk.js"
+  },
+  {
+    "revision": "33a52e7ba2c63c9d614a",
+    "url": "/KaramveerSidhu/robo-search/static/js/runtime-main.ef1efd12.js"
+  },
+  {
+    "revision": "64c0669ab50b3026bb9a3ae9406ef163",
+    "url": "/KaramveerSidhu/robo-search/static/media/cf-i-robot-font.64c0669a.ttf"
+  }
+]);
